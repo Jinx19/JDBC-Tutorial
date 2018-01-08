6c35d292deb66590973ffb8b2dd1d8cdce302f42
jinx19 <472580488@qq.com>
2018-01-08 19:31:23 +0800

    mysql connect success

---
+++ jdbc/src/main/java/net/tan/jdbc/App.java
@@ -0,0 +1,13 @@
+package net.tan.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jdbc/src/main/java/net/tan/jdbc/JDBCTest.java
@@ -0,0 +1,11 @@
+package net.tan.jdbc;
+
+public class JDBCTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JDBCConnect connect = new JDBCConnect();
+		connect.initDatabase();
+	}
+
+}
